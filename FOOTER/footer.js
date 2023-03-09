77e2e68d0a4d6c277b999f9bb1340f5d98cd9a13
Nikita Gupta <sanghinikita@gmail.com>
2023-03-09 15:09:56 +0530

    footer js edited

--- FOOTER/footer.js
+++ FOOTER/footer.js
@@ -1,11 +1,39 @@
-function footer(){
+function footer() {
     return `
     <div id="aboutFaballey">
             <div style="display:flex;">
                 <h4>ONLINE SHOPPING SITE FOR WOMEN’S FASHION - FABALLEY</h4>
                 <div class="borderFun1"></div>
             </div>
-            <p>FabAlley.com is an online fashion store for taste-makers and trend-breakers all over the country. When it comes to online shopping for women looking for the latest fashion trends, FabAlley is the one-stop destination. Started in 2012, this online fashion store comprises of the trendiest tops, hottest dresses, skirts, jackets, shoes, bags, accessories and fashion jewellery for women online shopping. Every new season brings fresh and latest fashion for women. And it becomes important for you to keep up with the current trend. However, FabAlley has all your styling stress sussed!</p>
+            <p>FabAlley.com is an online fashion store for taste-makers and trend-breakers all over the country. When it
+                comes to online shopping for women looking for the latest fashion trends, FabAlley is the one-stop
+                destination. Started in 2012, this online fashion store comprises of the trendiest tops, hottest
+                dresses, skirts, jackets, shoes, bags, accessories and fashion jewellery for women online shopping.
+                Every new season brings fresh and latest fashion for women. And it becomes important for you to keep up
+                with the current trend. However, FabAlley has all your styling stress sussed! <span id="dotss">...</span><br><br> <span id="read_more">Going out for a stroll along the streets? Or getting ready for an important meeting at work? Or simply watching Netflix at home, cuddled up with your loved one in bed? Whatever plans you have, one common decision you should take in all these cases is what to wear. Women are generally very particular about their outfits and latest fashion trends. FabAlley, an online fashion store, houses a colossal collection of Tops, Dresses, Skirts, Bottoms, Shorts, Blazers and Jackets, all bang-on trend and in all sizes, from petite to plus-size.
+
+                    <br><br>Going to a brick and mortar store and browsing your perfect outfit is very tedious and time consuming. It is physically impossible to check out all the latest fashion trends for women from various brands in the same day. In the fast-moving world, today, no one has this much time! This is one of the main reasons that Online Shopping for women is so popular these days. It’s much convenient, time-saving and offers a wider variety of surf too. It also lets you compare prices across different websites. From Instagram and Facebook to googling and scanning through several online fashion store India, the options for browsing are endless. FabAlley is one such online fashion store that is a one- stop shop for all your fashion needs, be it ethnic or western wear, bags or shoes. The best part being we provide the latest women online shopping trends.
+                    
+                    <br><br><span class="read_para">Online Shopping for Women in India</span>
+                    <br><br>Shopping is no longer an exhausting affair with women online shopping sites offering the accessibility of fuss-free shopping services, anytime and anywhere. With a larger than life repertoire to choose from, online shopping sites for women have made everyone’s life much easier and convenient. In this fast-paced world, Online Shopping is a boon. Be it branded or designer outfits, online shopping caters you the best of both. With services like cash on delivery, easy return, exchange & refund, these online stores are a fantastic platform that can create the best shopping experiences for people across the globe. Apart from providing the trendiest apparels and accessories, these online sites offer products at affordable prices without compromising on the quality of the item. Furthermore, services are available round the clock, no matter what time or day it is.
+                    
+                    <br><br><span class="read_para">FABALLEY: One-Stop Fashion Shop</span> 
+                    <br><br>Who doesn’t want to stay trendy? Everyone does. Synonymous with chic and glamorous, Faballey is an exclusive fashion online shopping store for women who are rebellious and love quirky and unconventional styling. Featuring season’s most trending outfits and much-in-fame accessories in the realm of fashion, FabAlley is the best online store for people who loves to own ensembles that are sexy, hot, cool and fashionable. Tops, Dresses, Skirts, Pants or Denims to name a few, Faballey has everything to offer, under one single roof. FabAlley is all about redefining the fashion in the chicest way. At FabAlley, you will be spoiled for choice. There is something fun and dramatic for every fashion aficionado out there. To make shopping easy and convenient, Faballey provides various facilities through online payment, Door Step Delivery and Cash on Delivery. In case you do not like the purchased product or have any kind of size issues, you can easily avail our Return, Refund and Exchange facility. Customer satisfaction is our utmost priority. And to ensure that all the queries and issues are resolved at the earliest, we have a customer care team to assist our esteemed customers.
+                    
+                    <br><br><span class="read_para">Shop from The Masaba And Alia for Faballey Collection</span>
+                    <br><br>FabAlley is undoubtedly the best online fashion store in India. If you are in search of designer outfits online, we bring you an exclusive range of ensembles designed by the renowned designer Masaba. The MISPRINT collection by Masaba exhibits eye-grabbing Tops, Bracelets, Flared Trousers, Bodycon Dresses, and Sandals in offbeat prints and designs that will surely sweep you off your feet. For those who are in love with minimalist styling, Alia for FabAlley Collection is something you must explore. Imbued with classic and contemporary detailing, this collection is perfect for the new-age ladies.
+                    
+                    <br><br><span class="read_para">Buy Women’s Wear Online on FabAlley</span>
+                    <br><br>Hello, Fashion devotees! It’s time for you to ride high on style with the most fashionable women’s clothing online store- FabAlley. Check out our stunning range of Dresses that are trending big time. Whether you’re looking for a Bodycon, Skater, A-line, Shift, Off Shoulder or Maxi Dresses, we have every type of dresses that is going to make you croon. For those who are inclined towards gothic and boho styling, we have Summer in Bohemia collection. Also, explore our vast array of sexy Tank Tops, stylish Shirts, hot blouses for girls , elegant Ruffled Tops, basics and many more. We are not just restricted to clothing, step up your fashion quotient with our cool range of accessories. From stylish Bags to beautiful Scarves, cool Sunglasses to cute Hair Accessories and swanky Jewelry, we unveil the best for you. In case you are a diehard fan of both ethnic and western styles, FabAlley have got you covered. Indya is exclusively for girls who loves to wear traditional and ethnic outfits with a dash of contemporary twist. Enjoy the best of Indo Western wear with FabAlley’s gorgeous collection of graceful Skirts, Peplums, Crop Tops for women, Tunics, Dresses, Palazzo et al. Apart from these amazing products, we also have a smart collection of Shoes for women, which consists of Peep-Toes, Flat Sandals, Lace-up Sandals, Strappy Flat Sandals, Block Heeled Sandals and Wedge Sandals. Choose yours!
+                    
+                    <br><br><span class="read_para">Online Shopping Advantages</span>
+                    <br><br>One of the best advantages of Online Shopping is that, you do not have to plan or invest your entire day running from one store to the other. With thousands of ladies wear online and the information on the website, shopping with Faballey is easy and time- saving. You can purchase your product depending on your taste in styling, favorite color, print or fabric on FabAlley. Apart from this, you get easy return and exchange facility as well. At FabAlley, we ensure on-time delivery so as not to create any fuss for our customers. To cater you a safe and stress-free online shopping experience, we have Cash on Delivery facility for you.
+                    
+                    <br><br><span class="read_para">Global Celebrities - #ALLEYGALS</span>
+                    <br><br>When it comes to online shopping sites for women outfits, FabAlley comprises the chicest apparel for girls, ranging from Tops, hottest Dresses, Shirts, Jackets, Skirts, Jeans, Blazers, Pants and many more. And when you slip into one of these outfits, we make sure that we get all the spotlight on you. Just tag us and get featured on our website. You go, girl!
+                    
+                    <br><br><span class="read_para">Our Popular Categories:</span> Women Tops | Women Dresses | Women Skirts | Women Bottoms Pants | Summer Collection for Women | Women Loungewear | Winter Wear for Women</span></p>
+                <div id="readmore_btn"><button id="button_toreadmore">Read more</button></div>
         </div>
         <!-- line... -->
         <div class="borderFun"></div>
@@ -63,7 +91,7 @@ function footer(){
                                 <p><a href="##">Shipping Policy</a></p>
                                 <p><a href="##">Ordering & Payment</a></p>
                                 <p><a href="##">Size Guide</a></p>
-                                <p><a href="##"></a>Wholesale Enquiry
+                                <p><a href="##">Wholesale Enquiry</a>
                             </a></p>
 
                     </div>
